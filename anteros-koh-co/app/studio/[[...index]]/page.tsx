0ea24695b42a35f37c3d16fa0eef40ef1153efc4
ryananteroskoh <ryananteroskoh@gmail.com>
2026-03-18 20:46:01 -0700

    Update page.tsx

--- anteros-koh-co/app/studio/[[...index]]/page.tsx
+++ anteros-koh-co/app/studio/[[...index]]/page.tsx
@@ -4,6 +4,7 @@ import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default function StudioPage() {
   return <NextStudio config={config} />
